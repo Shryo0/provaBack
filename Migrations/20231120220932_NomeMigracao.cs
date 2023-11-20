03a443d7bf8d7a37b73c52dcf31d2ad3e837666a
aluno <Aluno@PR02-302-33.CSED.LAB>
2023-11-20 19:17:17 -0300

    first commit

---
+++ Migrations/20231120220932_NomeMigracao.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace API.Migrations
+{
+    public partial class NomeMigracao : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Categorias",
+                columns: table => new
+                {
+                    CategoriaId = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Nome = table.Column<string>(type: "TEXT", nullable: true),
+                    CriadoEm = table.Column<DateTime>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Categorias", x => x.CategoriaId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Tarefas",
+                columns: table => new
+                {
+                    TarefaId = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Titulo = table.Column<string>(type: "TEXT", nullable: true),
+                    Status = table.Column<string>(type: "TEXT", nullable: false),
+                    Descricao = table.Column<string>(type: "TEXT", nullable: true),
+                    CriadoEm = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    CategoriaId = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Tarefas", x => x.TarefaId);
+                    table.ForeignKey(
+                        name: "FK_Tarefas_Categorias_CategoriaId",
+                        column: x => x.CategoriaId,
+                        principalTable: "Categorias",
+                        principalColumn: "CategoriaId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Categorias",
+                columns: new[] { "CategoriaId", "CriadoEm", "Nome" },
+                values: new object[] { 1, new DateTime(2023, 11, 21, 19, 9, 32, 466, DateTimeKind.Local).AddTicks(8271), "Trabalho" });
+
+            migrationBuilder.InsertData(
+                table: "Categorias",
+                columns: new[] { "CategoriaId", "CriadoEm", "Nome" },
+                values: new object[] { 2, new DateTime(2023, 11, 22, 19, 9, 32, 466, DateTimeKind.Local).AddTicks(8275), "Estudos" });
+
+            migrationBuilder.InsertData(
+                table: "Categorias",
+                columns: new[] { "CategoriaId", "CriadoEm", "Nome" },
+                values: new object[] { 3, new DateTime(2023, 11, 23, 19, 9, 32, 466, DateTimeKind.Local).AddTicks(8277), "Lazer" });
+
+            migrationBuilder.InsertData(
+                table: "Tarefas",
+                columns: new[] { "TarefaId", "CategoriaId", "CriadoEm", "Descricao", "Status", "Titulo" },
+                values: new object[] { 1, 1, new DateTime(2023, 11, 27, 19, 9, 32, 466, DateTimeKind.Local).AddTicks(8338), "Terminar relatório para reunião", "não iniciado", "Concluir relatório" });
+
+            migrationBuilder.InsertData(
+                table: "Tarefas",
+                columns: new[] { "TarefaId", "CategoriaId", "CriadoEm", "Descricao", "Status", "Titulo" },
+                values: new object[] { 2, 2, new DateTime(2023, 11, 23, 19, 9, 32, 466, DateTimeKind.Local).AddTicks(8340), "Preparar-se para a aula de Angular", "não iniciado", "Estudar Angular" });
+
+            migrationBuilder.InsertData(
+                table: "Tarefas",
+                columns: new[] { "TarefaId", "CategoriaId", "CriadoEm", "Descricao", "Status", "Titulo" },
+                values: new object[] { 3, 3, new DateTime(2023, 12, 4, 19, 9, 32, 466, DateTimeKind.Local).AddTicks(8341), "Dar um passeio relaxante no parque", "não iniciado", "Passeio no parque" });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Tarefas_CategoriaId",
+                table: "Tarefas",
+                column: "CategoriaId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Tarefas");
+
+            migrationBuilder.DropTable(
+                name: "Categorias");
+        }
+    }
+}
